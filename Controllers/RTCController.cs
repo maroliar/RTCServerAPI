716e64a0ab2d4b0c3fcb9ad2d10e8f88393b0c5a
Marcelo Oliveira <marcelouff@outlook.com>
2020-05-23 13:32:37 -0300

    Add project files.

---
+++ Controllers/RTCController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RTCServerAPI.Entities;
+
+namespace RTCServerAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RTCController : ControllerBase
+    {
+        private readonly ILogger<RTCController> _logger;
+
+        public RTCController(ILogger<RTCController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public string Get() 
+        {
+            return GetAndFormatJsonDateTime();
+        }
+
+        private string GetAndFormatJsonDateTime()
+        {
+            var dateTime = DateTime.Now;
+
+            var dateTimeFormatted = new DateTimeFormatted
+            {
+                day = dateTime.Day,
+                month = dateTime.Month,
+                year = dateTime.Year,
+                hour = dateTime.Hour,
+                minute = dateTime.Minute,
+                second = dateTime.Second
+            };
+
+            return JsonSerializer.Serialize(dateTimeFormatted);
+        }
+    }
+}
---
+++ Entities/DateTimeFormatted.cs
@@ -0,0 +1,12 @@
+﻿namespace RTCServerAPI.Entities
+{
+    public class DateTimeFormatted
+    {
+        public int day { get; set; }
+        public int month { get; set; }
+        public int year { get; set; }
+        public int hour { get; set; }
+        public int minute { get; set; }
+        public int second { get; set; }
+    }
+}
